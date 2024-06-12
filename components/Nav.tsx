b017fc71a18c1613eabb2af5b695b89e401a738d
retr0717 <alwinaji717@gmail.com>
2024-06-12 15:07:16 +0530

    nav links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,46 @@
+'use-client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+
+]
 
 const Nav = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
-    <nav>
-      <h1>Desktop Nav</h1>
-    </nav>
+    <nav className="flex gap-8">
+      {
+        links.map((link: { name: string, path: string }, index: number) => (
+          <Link href={link.path} key={index} className={`${pathname == link.path ? "text-accent border-b-2 border-accent" : ""}`} >
+            {link.name}
+          </Link>
+        ))
+      }
+    </nav >
   );
 };
 
