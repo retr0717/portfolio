869ad3486df0e1ce4bc47c0d1a97e694e80b2b09
retr0717 <alwinaji717@gmail.com>
2024-06-15 16:21:31 +0530

    profile pic with transition and ordering according to screen size

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 const Home = () => {
   return (
     <section className="h-full">
@@ -8,7 +9,7 @@ const Home = () => {
         <div>
           <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
             {/*text*/}
-            <div className="text-center xl:text-left">
+            <div className="text-center xl:text-left order-2 xl:order-none">
               <span className="text-xl">Full Stack Developer</span>
               <h1 className="h1 mb-6">
                 Hola I'm <br />
@@ -38,7 +39,9 @@ const Home = () => {
               </div>
             </div>
             {/*photo*/}
-            <div>Photo</div>
+            <div className="order-1 xl:order-none mb-8 xl:mb-0">
+              <Photo />
+            </div>
           </div>
         </div>
       </div>
