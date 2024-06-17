0a719104ca3ffb3f394b06168abdfef4c986d595
retr0717 <alwinaji717@gmail.com>
2024-06-17 07:56:13 +0530

    work page : project slider button done

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/tooltip";
 import Link from "next/link";
 import Image from "next/image";
+import WorkSliderButtons from "@/components/WorkSliderBtns";
 
 const projects = [
   {
@@ -165,7 +166,10 @@ const Work = () => {
                 );
               })}
               {/*slider buttons*/}
-              <WorkSliderButtons />
+              <WorkSliderButtons
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
             </Swiper>
           </div>
         </div>
--- components/WorkSliderBtns.tsx
+++ components/WorkSliderBtns.tsx
@@ -1,5 +1,26 @@
-const WorkSliderButtons = () => {
-  return <div></div>;
+"use client";
+
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+const WorkSliderButtons = ({
+  containerStyles,
+  btnStyles,
+}: {
+  containerStyles: any;
+  btnStyles: any;
+}) => {
+  const swiper = useSwiper();
+  return (
+    <div className={containerStyles}>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretLeftBold />
+      </button>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretRightBold />
+      </button>
+    </div>
+  );
 };
 
 export default WorkSliderButtons;
